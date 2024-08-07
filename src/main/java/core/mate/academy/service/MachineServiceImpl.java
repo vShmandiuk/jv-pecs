6a66d7a71206215e277fbcfa6d7b07040c3e139d
Viktoriia <shmandiuk.grizzly@gmail.com>
2024-08-07 13:30:29 +0200

    Create classes and implement

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -13,15 +13,19 @@ public class MachineServiceImpl implements MachineService<Machine> {
     @Override
     public List<Machine> getAll(Class<? extends Machine> type) {
         List<? extends Machine> machines;
-        if (type == Bulldozer.class) {
-            machines = new BulldozerProducer().get();
-        } else if (type == Excavator.class) {
-            machines = new ExcavatorProducer().get();
-        } else if (type == Truck.class) {
-            machines = new TruckProducer().get();
-        } else {
-            List<Machine> emptyList = Collections.emptyList();
-            return emptyList;
+        switch (type) {
+            case Bulldozer:
+                machines = new BulldozerProducer().get();
+                break;
+            case Excavator:
+                machines = new ExcavatorProducer().get();
+                break;
+            case Truck:
+                machines = new TruckProducer().get();
+                break;
+            default:
+                machines = Collections.emptyList();
+                break;
         }
         return new ArrayList<>(machines);
     }

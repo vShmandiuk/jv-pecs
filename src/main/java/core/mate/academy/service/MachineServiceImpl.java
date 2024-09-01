56db0e816925b250abbee13c8254bd597cd3fb90
Viktoriia <shmandiuk.grizzly@gmail.com>
2024-09-01 14:37:07 +0200

    zrobiłam switch i dodałam znaczenia do aut

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,10 +1,6 @@
 package core.mate.academy.service;
 
-import core.mate.academy.model.Bulldozer;
-import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
-import core.mate.academy.model.Truck;
-import core.mate.academy.model.Workable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
